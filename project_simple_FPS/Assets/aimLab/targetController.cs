fb68e772a4ec3687d31468cc82103b212263d7ff
JaeHyunAn <98042706+yyuneu@users.noreply.github.com>
2025-01-11 03:26:21 +0900

    add enemy skin and aimlab update
    ㅈㄱㄴ

--- project_simple_FPS/Assets/aimLab/targetController.cs
+++ project_simple_FPS/Assets/aimLab/targetController.cs
@@ -6,30 +6,34 @@ public class targetControoler : MonoBehaviour, IDamagable
 
     [SerializeField]
     private float hp = 20f;
-    
+
     public aimLabController controller;
     public GameObject startTarget;
+    public generator generator;
 
     private void Start()
     {
         controller = Object.FindAnyObjectByType<aimLabController>();
+        generator = Object.FindAnyObjectByType<generator>();
     }
     public void TakeHit(float damage)
     {
 
         hp -= damage;
         if (hp <= 0)
-        {   
+        {
             if (gameObject == startTarget)
             {
                 controller.StartGame();         //startTarget 사격시 게임시작
             }
             else
             {
-                
+
                 controller.IncreaseScore();     //target 사격시 점수증가
+                generator.setPos(transform.position.x, transform.position.y, transform.position.z);
+
             }
-           
+
             Destroy(gameObject);
         }
     }
